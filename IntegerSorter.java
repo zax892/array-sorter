32bbb1dec3dece82b9a337796e74196ba849e4d4
zax892 <zachblue12@hotmail.com>
2024-11-14 14:18:10 -0800

    added Inline comment for bubble sort

--- IntegerSorter.java
+++ IntegerSorter.java
@@ -1,7 +1,7 @@
 public class IntegerSorter {
 
     public int[] sortIntegers(int[] array) {
-        // Sorting logic (simple bubble sort for demonstration)
+        // Bubble sort is used to sort integers in ascending order
         for (int i = 0; i < array.length - 1; i++) {
             for (int j = i + 1; j < array.length; j++) {
                 if (array[i] > array[j]) {
